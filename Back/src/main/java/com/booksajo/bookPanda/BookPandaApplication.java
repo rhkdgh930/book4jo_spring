40ba43a0e09768a2472c1c0b45daa09470414182
wooseok <gowoo97@gmail.com>
2024-05-28 11:49:13 +0900

    init

---
+++ Back/src/main/java/com/booksajo/bookPanda/BookPandaApplication.java
@@ -0,0 +1,13 @@
+package com.booksajo.bookPanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookPandaApplication.class, args);
+	}
+
+}
---
+++ Back/src/test/java/com/booksajo/bookPanda/BookPandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booksajo.bookPanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
