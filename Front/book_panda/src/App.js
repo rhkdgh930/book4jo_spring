bdad2947418e590164fa510a176a608f55cfdccc
myeong kwangho <rhkdgh99093015@gmail.com>
2024-05-31 06:34:22 +0900

    Feat: add iamport

--- Front/book_panda/src/App.js
+++ Front/book_panda/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const Payment = () => {
+  useEffect(() => {
+    const jquery = document.createElement("script");
+    jquery.src = "http://code.jquery.com/jquery-1.12.4.min.js";
+    const iamport = document.createElement("script");
+    iamport.src = "http://cdn.iamport.kr/js/iamport.payment-1.2.0.js";
+    document.head.appendChild(jquery);
+    document.head.appendChild(iamport);
+    return () => {
+      document.head.removeChild(jquery);
+      document.head.removeChild(iamport);
+    };
+  }, []);
+
+  const requestPay = () => {
+    const { IMP } = window;
+    IMP.init('imp14170881');
+
+    IMP.request_pay({
+      pg: 'html5_inicis',
+      pay_method: 'card',
+      merchant_uid: new Date().getTime(),
+      name: '테스트 상품',
+      amount: 200,
+      buyer_email: 'test@naver.com',
+      buyer_name: '코드쿡',
+      buyer_tel: '010-1234-5678',
+      buyer_addr: '서울특별시',
+      buyer_postcode: '123-456',
+    }, async (rsp) => {
+      try {
+        const { data } = await axios.post('http://localhost:3000/verifyIamport/' + rsp.imp_uid);
+        if (rsp.paid_amount === data.response.amount) {
+          alert('결제 성공');
+        } else {
+          alert('결제 실패');
+        }
+      } catch (error) {
+        console.error('Error while verifying payment:', error);
+        alert('결제 실패');
+      }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.orgqwe"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={requestPay}>결제하기</button>
     </div>
   );
-}
+};
 
-export default App;
+export default Payment;
