72e8c7c097d0a533ad734caf5bc770b63d4c8db4
wooseok <gowoo97@gmail.com>
2024-05-30 14:01:57 +0900

    feat: 리액트 라우터 적용

--- Front/book_panda/src/App.js
+++ Front/book_panda/src/App.js
@@ -3,15 +3,20 @@ import Header  from './components/Header';
 import SearchBar from './components/SearchBar';
 import Nav from './components/Nav';
 import Main from './pages/Main';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+     
         <Header></Header>
         <SearchBar></SearchBar>
         <Nav></Nav>
-        
-        <Main></Main>
+        <Routes>
+        <Route path="/" element={<Main/>}></Route>
+        </Routes>
+        </BrowserRouter>
     </div>
   );
 }
