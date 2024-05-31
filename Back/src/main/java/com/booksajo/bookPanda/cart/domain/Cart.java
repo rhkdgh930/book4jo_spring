0d1a0815bbea4bfc8c38792517b955d52d97e01f
[Cloud3] 최고은 <oh-my@nate.com>
2024-05-31 21:54:20 +0900

    feat: 장바구니 URI 변경

--- Back/src/main/java/com/booksajo/bookPanda/cart/domain/Cart.java
+++ Back/src/main/java/com/booksajo/bookPanda/cart/domain/Cart.java
@@ -1,5 +1,6 @@
 package com.booksajo.bookPanda.cart.domain;
 
+import com.booksajo.bookPanda.user.domain.User;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -15,20 +16,20 @@ public class Cart {
     @Column(name = "id", nullable = false)
     private Long id;
 
-//    @OneToOne
-//    @JoinColumn(name = "user_id")
-//    private User user;
-//
-//    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
-//    private List<CartItem> cartItems = new ArrayList<>();
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
-//    public void addItem(CartItem cartItem) {
-//        cartItems.add(cartItem);
-//        cartItem.setCart(this);
-//    }
-//
-//    public void removeItem(CartItem cartItem) {
-//        cartItems.remove(cartItem);
-//        cartItem.setCart(null);
-//    }
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> cartItems = new ArrayList<>();
+
+    public void addItem(CartItem cartItem) {
+        cartItems.add(cartItem);
+        cartItem.setCart(this);
+    }
+
+    public void removeItem(CartItem cartItem) {
+        cartItems.remove(cartItem);
+        cartItem.setCart(null);
+    }
 }
